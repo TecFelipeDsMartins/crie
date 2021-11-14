45b2ca47e919e4d347af59d0661a02b2f52d5089
Felipe Martins <tec.felipedsmartins@gmail.com>
2021-11-14 00:14:58 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,63 +3,44 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Empresas Crie</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="bg-header-patern bg-no-repeat md:bg-fixed bg-top md:bg-center bg-cover md:h-auto h-40 md:w-full flex md:flex-row flex-col justify-center items-center md:mb-9 mb-36 " >
+       <div className="w-1/2 h-40 md:h-96 md:mb-20">
+       </div>
+       <div className="md:w-1/2 flex flex-col justify-center items-center relative top-32 md:-top-8">
+        <h1 className="text-xl text-gray-600 pb-3">Buscar empresa por categoria</h1>
+        <select className="w-72 text-base" name="empresas" id="emp">
+          <option value="limpeza" disabled>Escolha uma categoria</option>
+          <option value="limpeza">Limpeza</option>
+          <option value="saude">Saúde</option>
+          <option value="comunicação">Comunicação</option>
+          <option value="contabilidade">Financeiro</option>
+        </select>
+       </div> 
+      </div>
+      <div className="h-auto w-full flex flex-col justify-center items-center" >
+        
+        {/* todo make component */}
+        <div className="flex flex-col md:flex-row justify-center items-center py-16 border-b md:w-5/6 ">
+          <div className="px-6 md:w-2/6 flex flex-col md:justify-start justify-center items-center  ">
+            <img className="w-64 h-64 mb-8 md:mb-0 rounded-lg shadow-xl " src="img/criefundo.png" alt="" />                        
+          </div>
+          <div className="w-4/6 h-auto text-center md:text-left ">
+            <h2 className="text-4xl pb-2">Categoria</h2>
+            <h3 className="text-xl text-gray-600 pb-5">nome da empresa</h3>
+            <p className="pb-8 text-xl md:text-base ">Iure dolore consequatur pariatur rem tenetur aperiam, enim quos corporis eligendi hic ab temporibus. Optio tempora molestias ducimus incidunt nostrum id vero quae dolorem repudiandae tempore, nulla eius animi culpa rem sit laudantium assumenda pariatur mollitia provident accusamus, sapiente odit? Consequatur culpa quos voluptatem nihil inventore, ducimus dolorum ad aperiam distinctio labore explicabo voluptates libero et qui.</p>
+            <a href="">
+              <button className="border rounded-md w-44 h-10 md:w-28 md:h-8 text-xl md:text-base shadow hover:shadow-none bg-green-500 text-white">Detalhes</button>
+            </a> 
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        {/*  */}
+    
+      </div> 
+    </>
   )
+  
 }
